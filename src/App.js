d4a9ed3720c2db43fe3e15cf120e7218d51c985a
Anushka <95957335+anushka3002@users.noreply.github.com>
2023-03-18 13:49:51 +0530

    implemented chat box

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       <Router/>
       </div>
     </div>
-    <Chat/>
+    {location.pathname!=="/" && <Chat/>}
     </UserContext.Provider>
   );
 }
--- src/component/chat.jsx
+++ src/component/chat.jsx
@@ -1,54 +1,119 @@
-import React, { useContext, useState } from 'react'
-import { UserContext } from '../App';
+import React, { useContext, useState } from "react";
+import { UserContext } from "../App";
 
 const Chat = () => {
-    const [showList,setShowList] = useState(false)
-    let user = JSON.parse(localStorage.getItem("panorbit user")) || []
-    let users = JSON.parse(localStorage.getItem("all users")) || []
-    console.log(users,"users")
+  let user = JSON.parse(localStorage.getItem("panorbit user")) || [];
+  let users = JSON.parse(localStorage.getItem("all users")) || [];
+  const [showList, setShowList] = useState(false);
+  const [showChat,setShowChat] = useState(false)
+  const [chatUser, setChatUser] = useState(user);
+  console.log(users, "users");
 
   return (
-    <div className='border'>
-    <div className='w-full flex justify-end fixed bottom-0'>
-    <div className='flex border border-[red]'>
-        <div className='bg-[#2c65c8] cursor-pointer flex py-2 w-full flex rounded-t-[12px]'>
-            
+    <div className="border">
+      <div className="w-full flex justify-end fixed bottom-0">
+        <div className={`${showChat?"visible":"hidden"} border border-[#2c65c8] bg-[white] rounded-t-[12px] fixed bottom-0 mr-[250px]`}>
+          <div onClick={()=>setShowChat(!showChat)} className="bg-[#2c65c8] cursor-pointer py-2 rounded-t-[12px]">
+            <div className="flex px-2 justify-between">
+              <div className="flex">
+                <img
+                  className="w-[27px] rounded-[50%] mr-3"
+                  src={chatUser.profilepicture}
+                ></img>
+                <p className="text-[white] text-[12px] my-auto">
+                  {chatUser.name}
+                </p>
+              </div>
+              <div className="flex">
+                <img
+                  className="w-[12px] h-[7px] flex my-auto"
+                  src="https://www.seekpng.com/png/full/914-9148298_downarrow-white-down-arrow-icon-white.png"
+                ></img>
+                <img
+                  className="w-[14px] h-[10px] flex my-auto ml-2"
+                  src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/letter-x-icon-18-256.png"
+                ></img>
+              </div>
+            </div>
+          </div>
+          <div className="border-b">
+            <p className="text-[12px] p-3 bg-[#f1f1f1] w-[70%] ml-3 mt-3 mb-1">
+              Hi, how are you
+            </p>
+            <p className="text-[12px] p-3 bg-[#f1f1f1] w-[70%] ml-3 mt-3 mb-1">
+              How is it going?
+            </p>
+            <p className="text-[12px] py-1 text-center text-[#a2a2a2]">
+              9:16 pm
+            </p>
+            <p className="text-[12px] p-3 bg-[#f1f1f1] w-[70%] ml-10 mt-3 mb-1">
+              Hey, I am good, what about you?
+            </p>
+            <p className="text-[12px] p-3 bg-[#f1f1f1] w-[70%] ml-10 mt-3 mb-1">
+              I am also good.
+            </p>
+          </div>
+          <div className="flex">
+            <input className="w-[90%]"></input>
+            <img
+              className="w-[14px] h-[14px] w-[10%] my-auto"
+              src="https://cdn-icons-png.flaticon.com/512/3682/3682321.png"
+            ></img>
+          </div>
         </div>
-    </div>
-    <div className='border mr-12 w-[200px] border-[#2c65c8] rounded-t-[12px]'>
-        <div onClick={()=>setShowList(!showList)} className=' bg-[#2c65c8] cursor-pointer flex py-2 w-full flex rounded-t-[12px]'>
-        <img className='w-[30px] h-[20px] ml-1' src="/chat.png"></img>
-        <p className='text-[white]'>Chats</p>
-        <div className='my-auto justify-end'>
-        <img className='w-[15px] h-[7px] flex my-auto' src="https://www.seekpng.com/png/full/914-9148298_downarrow-white-down-arrow-icon-white.png"></img>
-        </div></div>
-        <div className={` ${showList?"visible":"hidden"} px-2 overflow-y-scroll h-[370px] scroll-panorbit`}>
-          {users.map((e) => {
-            return (
-            //   <Link to="/profile">
+        <div className="border mr-12 w-[200px] border-[#2c65c8] rounded-t-[12px]">
+          <div
+            onClick={() =>{ setShowList(!showList);setShowChat(false)}}
+            className=" bg-[#2c65c8] cursor-pointer justify-between px-3 flex py-2 w-full flex rounded-t-[12px]"
+          >
+            <div className="flex">
+              <img className="w-[30px] h-[20px] my-auto" src="/chat.png"></img>
+              <p className="text-[white]">Chats</p>
+            </div>
+            <div className="my-auto">
+              <img
+                className="w-[15px] h-[7px] flex my-auto"
+                src="https://www.seekpng.com/png/full/914-9148298_downarrow-white-down-arrow-icon-white.png"
+              ></img>
+            </div>
+          </div>
+          <div
+            className={` ${
+              showList ? "visible" : "hidden"
+            } px-2 overflow-y-scroll h-[370px] scroll-panorbit bg-[white]`}
+          >
+            {users.map((e) => {
+              return (
                 <div
-                //   onClick={() => handleClick(e)}
+                  onClick={() =>{setChatUser(e);setShowChat(true)}}
                   key={e.id}
                   className="flex py-1 cursor-pointer justify-between"
                 >
-                    <div className='flex'>
-                  <img
-                    className="w-[30px] h-[30px] rounded-[50%] mr-3"
-                    src={e.profilepicture}
-                  ></img>
-                  <p className="text-[#1c2938] text-[13px] my-auto">{e.name}</p>
+                  <div className="flex">
+                    <img
+                      className="w-[30px] h-[30px] rounded-[50%] mr-3"
+                      src={e.profilepicture}
+                    ></img>
+                    <p className="text-[#1c2938] text-[13px] my-auto">
+                      {e.name}
+                    </p>
                   </div>
-                  <div className={`${(e.id==2 || e.id ==user.id || e.id ==3 || e.id ==7) ? "bg-[#1eaa5c]" : "bg-[#d4d4d4]" } w-[7px] h-[7px] rounded-[50%] my-auto`}></div>
+                  <div
+                    className={`${
+                      e.id == 2 || e.id == user.id || e.id == 3 || e.id == 7
+                        ? "bg-[#1eaa5c]"
+                        : "bg-[#d4d4d4]"
+                    } w-[7px] h-[7px] rounded-[50%] my-auto`}
+                  ></div>
                 </div>
-            //   </Link>
-            );
-          })}
+                //   </Link>
+              );
+            })}
+          </div>
         </div>
-        </div>
-        </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
